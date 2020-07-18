7dd4d08a85948fced64ea3f71596f40a0b7e4622
danil1356 <68468188+danil1356@users.noreply.github.com>
2020-07-18 15:40:01 +0300

    Add files via upload

---
+++ my.js
@@ -0,0 +1,93 @@
+var
+nanimateli = 
+{
+nanimatel1: 'а',
+nanimatel2: 'б',
+}
+console.log(nanimateli);  
+
+
+alert('Добро пожаловать в отдел кадров');
+
+var selectt = document.querySelector('select');
+var pap = document.querySelector ('p');
+ 
+function dapt()
+{
+var choo = woow.value;
+var chooo = wooow.value;
+
+if (choo === 'nap1')
+{
+	if (chooo === 'napp1')
+	{
+		document.getElementById('wot').innerHTML =  nanimateli.nanimatel1;
+	}
+	else if(chooo === 'napp2')
+	{
+		document.getElementById('wot').innerHTML =  wot = document.getElementById('woow').value;
+	}
+	else if(chooo === 'napp3')
+	{
+		document.getElementById('wot').innerHTML =  wot = document.getElementById('woow').value;
+	}
+}
+else if (choo === 'nap2')
+{
+		if (chooo === 'napp1')
+	{
+		document.getElementById('wot').innerHTML =  wot = document.getElementById('woow').value;
+	}
+	else if(chooo === 'napp2')
+	{
+		document.getElementById('wot').innerHTML =  wot = document.getElementById('woow').value;
+	}
+	else if(chooo === 'napp3')
+	{
+		document.getElementById('wot').innerHTML =  wot = document.getElementById('woow').value;
+	}
+}
+else if (choo === 'nap3')
+{
+		if (chooo === 'napp1')
+	{
+		document.getElementById('wot').innerHTML =  nanimateli.nanimatel2;
+	}
+	else if(chooo === 'napp2')
+	{
+		document.getElementById('wot').innerHTML =  wot = document.getElementById('woow').value;
+	}
+	else if(chooo === 'napp3')
+	{
+		document.getElementById('wot').innerHTML =  wot = document.getElementById('woow').value;
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////
+/* if (chooo === 'napp1')
+{
+	document.getElementById('woot').innerHTML =  woot = document.getElementById('wooow').value;
+}
+else if (chooo === 'napp2')
+{
+	document.getElementById('woot').innerHTML =  woot = document.getElementById('wooow').value;
+}
+else if (chooo === 'napp3')
+{
+	document.getElementById('woot').innerHTML =  woot = document.getElementById('wooow').value;
+} */
+
+} 
+
+//////////////////////////////////////
+function rabota()
+{
+var 
+	namee = document.getElementById('1').value;	
+	familiaa = document.getElementById('2').value;	
+	otchestvoo = document.getElementById('3').value;
+	document.getElementById('infa').innerHTML = familiaa + " "+namee+ " "+otchestvoo+ " ,Вам подходят следующие вакансии: ";
+}
+
+
+
+
